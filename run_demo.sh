06699d59f58f37ae42fe7c670e9f108717f73706
Christophe Haen <christophe.haen@cern.ch>
2023-06-23 16:52:04 +0200

    fixing kind deployment

--- run_demo.sh
+++ run_demo.sh
@@ -3,14 +3,16 @@ set -euo pipefail
 IFS=$'\n\t'
 
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-tmp_dir=$(mktemp -d)
+
+tmp_dir="${script_dir}/.demo"
+mkdir -p "${tmp_dir}"
 export KUBECONFIG="${script_dir}/kube.conf"
 
 function cleanup(){
     trap - SIGTERM;
     echo "Cleaning up";
-    if [[ -f "${script_dir}/kind" ]] && [[ -f "${KUBECONFIG}" ]]; then
-        "${script_dir}/kind" delete cluster --name diracx-demo
+    if [[ -f "${tmp_dir}/kind" ]] && [[ -f "${KUBECONFIG}" ]]; then
+        "${tmp_dir}/kind" delete cluster --name diracx-demo
     fi
     rm -rf "${tmp_dir}"
 }
@@ -18,7 +20,7 @@ function cleanup(){
 trap "cleanup" EXIT
 
 
-if [[ ! -f "${script_dir}/helm" ]]; then
+if [[ ! -f "${tmp_dir}/helm" ]]; then
     # Inspect the current system
     system_name=$(uname -s | tr '[:upper:]' '[:lower:]')
     system_arch=$(uname -m)
@@ -28,44 +30,63 @@ if [[ ! -f "${script_dir}/helm" ]]; then
 
     # Download kind
     printf "\U1F984 Downloading kind\n"
-    curl --no-progress-meter -L "https://kind.sigs.k8s.io/dl/v0.19.0/kind-${system_name}-${system_arch}" > "${script_dir}/kind"
+    curl --no-progress-meter -L "https://kind.sigs.k8s.io/dl/v0.19.0/kind-${system_name}-${system_arch}" > "${tmp_dir}/kind"
 
     # Download kubectl
     printf "\U1F984 Downloading kubectl\n"
     latest_version=$(curl -L -s https://dl.k8s.io/release/stable.txt)
-    curl --no-progress-meter -L "https://dl.k8s.io/release/${latest_version}/bin/${system_name}/${system_arch}/kubectl" > "${script_dir}/kubectl"
+    curl --no-progress-meter -L "https://dl.k8s.io/release/${latest_version}/bin/${system_name}/${system_arch}/kubectl" > "${tmp_dir}/kubectl"
 
     # Download helm
     printf "\U1F984 Downloading helm\n"
     curl --no-progress-meter -L "https://get.helm.sh/helm-v3.12.0-${system_name}-${system_arch}.tar.gz" > "${tmp_dir}/helm.tar.gz"
-    mkdir -p "${tmp_dir}/helm"
-    tar xzf "${tmp_dir}/helm.tar.gz" -C "${tmp_dir}/helm"
-    mv "${tmp_dir}/helm/${system_name}-${system_arch}/helm" "${script_dir}"
+    mkdir -p "${tmp_dir}/helm-tarball"
+    tar xzf "${tmp_dir}/helm.tar.gz" -C "${tmp_dir}/helm-tarball"
+    mv "${tmp_dir}/helm-tarball/${system_name}-${system_arch}/helm" "${tmp_dir}"
 
     # Make the binaries executable
-    chmod +x "${script_dir}/kubectl" "${script_dir}/kind" "${script_dir}/helm"
+    chmod +x "${tmp_dir}/kubectl" "${tmp_dir}/kind" "${tmp_dir}/helm"
 fi
-./kind create cluster \
+"${tmp_dir}/kind" create cluster \
     --kubeconfig "${KUBECONFIG}" \
     --wait "1m" \
     --config "${script_dir}/demo_cluster_conf.yaml" \
     --name diracx-demo
 
 # run an Ingress
-./kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+"${tmp_dir}/kubectl" apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+"${tmp_dir}/kubectl" wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
 
 
-until (./kubectl get nodes | grep -c Ready) &>/dev/null; do
-    echo "Waiting for node to be ready";
-    sleep 1;
-done
 
-./helm install diracx-demo "${script_dir}/diracx"
+"${tmp_dir}/helm" install diracx-demo "${script_dir}/diracx"
 
-until (./kubectl get pods | grep diracx | grep -c Running) &>/dev/null; do
-    echo "Waiting for pods to be Running";
-    sleep 1;
-done
+"${tmp_dir}/kubectl" wait \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/name=diracx \
+  --timeout=180s
+
 
 echo "Pods are ready"
-sleep 1000
+echo ""
+echo "To interact with the cluster:"
+echo "export KUBECONFIG=${KUBECONFIG}"
+echo "export PATH=${PATH}:${tmp_dir}"
+echo ""
+echo "Edit your /etc/hosts file to contain"
+echo "127.0.0.1     localhost diracx-demo diracx-demo-dex"
+echo ""
+echo "You can access swagger at http://diracx-demo:8000/docs"
+echo "Username: admin@example.com"
+echo "Password: password"
+echo ""
+echo "Press Ctrl+C to exit"
+
+while [[ true ]];
+do
+    sleep 1000;
+done
