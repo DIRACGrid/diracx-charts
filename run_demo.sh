21880d5cc7e9db6edbedcdc4e9f4eaa3c371f5ca
Christophe Haen <christophe.haen@cern.ch>
2023-06-28 13:25:06 +0200

    Working with local hostname

--- run_demo.sh
+++ run_demo.sh
@@ -6,7 +6,7 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 tmp_dir="${script_dir}/.demo"
 mkdir -p "${tmp_dir}"
-export KUBECONFIG="${script_dir}/kube.conf"
+export KUBECONFIG="${tmp_dir}/kube.conf"
 
 function cleanup(){
     trap - SIGTERM;
@@ -14,7 +14,7 @@ function cleanup(){
     if [[ -f "${tmp_dir}/kind" ]] && [[ -f "${KUBECONFIG}" ]]; then
         "${tmp_dir}/kind" delete cluster --name diracx-demo
     fi
-    rm -rf "${tmp_dir}"
+    #rm -rf "${tmp_dir}"
 }
 
 trap "cleanup" EXIT
@@ -47,10 +47,23 @@ if [[ ! -f "${tmp_dir}/helm" ]]; then
     # Make the binaries executable
     chmod +x "${tmp_dir}/kubectl" "${tmp_dir}/kind" "${tmp_dir}/helm"
 fi
+
+echo "Generating Kind cluster template..."
+
+DIRACX_SRC_DIR=/home/chaen/dirac/diracx-project/diracx/src/diracx
+sed "s@{{ diracx_src_dir }}@${DIRACX_SRC_DIR}@g"  "${script_dir}/demo/demo_cluster_conf.tpl.yaml"  > "${tmp_dir}/demo_cluster_conf.yaml"
+
+if grep '{{' "${tmp_dir}/demo_cluster_conf.yaml"
+then
+  echo "Error generating Kind template. Found {{ in the template result"
+  exit 1
+fi
+
+
 "${tmp_dir}/kind" create cluster \
     --kubeconfig "${KUBECONFIG}" \
     --wait "1m" \
-    --config "${script_dir}/demo_cluster_conf.yaml" \
+    --config "${tmp_dir}/demo_cluster_conf.yaml" \
     --name diracx-demo
 
 # run an Ingress
@@ -62,13 +75,25 @@ fi
   --timeout=90s
 
 
+echo "Generating Helm templates"
 
-"${tmp_dir}/helm" install diracx-demo "${script_dir}/diracx"
+sed "s/{{ hostname }}/$(hostname)/g"  "${script_dir}/demo/values.tpl.yaml"  > "${tmp_dir}/values.yaml"
+
+if grep '{{' "${tmp_dir}/values.yaml"
+then
+  echo "Error generating template. Found {{ in the template result"
+  exit 1
+fi
+
+
+"${tmp_dir}/helm" install diracx-demo "${script_dir}/diracx" --values "${tmp_dir}/values.yaml"
+
+echo "Waiting for Installation to finish..."
 
 "${tmp_dir}/kubectl" wait \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/name=diracx \
-  --timeout=180s
+  --timeout=300s
 
 
 echo "Pods are ready"
@@ -76,9 +101,6 @@ echo ""
 echo "To interact with the cluster:"
 echo "export KUBECONFIG=${KUBECONFIG}"
 echo "export PATH=${PATH}:${tmp_dir}"
-echo ""
-echo "Edit your /etc/hosts file to contain"
-echo "127.0.0.1     localhost diracx-demo diracx-demo-dex"
 echo ""
 echo "You can access swagger at http://diracx-demo:8000/docs"
 echo "Username: admin@example.com"
