a665d218db23decb7471902e8ceb52abce74e8ad
aldbr <aldbr@outlook.com>
2024-12-10 18:20:41 +0100

    fix(diracx-web): support absolute paths

--- run_demo.sh
+++ run_demo.sh
@@ -267,7 +267,7 @@ for src_dir in "$@"; do
 
       # Do we find subdirectories called the same way as the package
       if [ -n "$(find "${src_dir}" -mindepth 3 -maxdepth 3 -type d  -path "*src/${pkg_name}")" ]; then
-      # And are there mulftiple pyprojects
+      # And are there multiple pyprojects
         if [ -n "$(find "${src_dir}" -mindepth 2 -maxdepth 2 -type f  -name "pyproject.toml")" ]; then
           # Then let's add all these
           while IFS= read -r  sub_pkg_dir
@@ -275,8 +275,8 @@ for src_dir in "$@"; do
             diracx_and_extensions_pkgs+=("$(basename "${src_dir}")/$(basename "${sub_pkg_dir}")");
           done < <(find "${src_dir}" -mindepth 1 -maxdepth 1 -type d -name "${pkg_name}-*" )
 
-          continue;
-        fi;
+          continue
+        fi
       fi
 
   fi
@@ -301,11 +301,26 @@ for src_dir in "$@"; do
   fi
 
   # Node packages: we keep a single package, the last one found
+  node_pkg_path=""
   while IFS='' read -r pkg_json; do
-    node_pkg_name="$(basename "$(dirname "${pkg_json}")")"
+    node_pkg_path=$src_dir
   done < <(find "$src_dir" -mindepth 1 -maxdepth 1 -type f -name 'package.json')
-done
+  node_pkg_name="$(basename "${node_pkg_path}")"
 
+  pkg_json="${node_pkg_path}/package.json"
+
+  # Check for workspaces in the package.json
+  if [ "$(jq -e ".workspaces | type== \"array\"" "$pkg_json")" == "true" ]; then
+    readarray -t node_pkg_workspaces < <(jq -r ".workspaces[]" "$pkg_json")
+    node_pkg_workspaces=("${node_pkg_workspaces[@]}")
+  fi
+
+  # Ensure node_modules exist, else create them, as volumes will be mounted there
+  mkdir -p "${node_pkg_path}"/node_modules
+  for workspace in "${node_pkg_workspaces[@]}"; do
+    mkdir -p "${node_pkg_path}/${workspace}"/node_modules
+  done
+done
 
 if [ ${#diracx_and_extensions_pkgs[@]} -gt 0 ]; then
   printf "%b Found Diracx/Extensions package directories for: %s\n" ${UNICORN_EMOJI} "${diracx_and_extensions_pkgs[*]}"
@@ -315,21 +330,9 @@ if [ ${#python_pkg_names[@]} -gt 0 ]; then
 fi
 if [ "${node_pkg_name}" != "" ]; then
   printf "%b Found Node package directory for: %s\n" ${UNICORN_EMOJI} "${node_pkg_name}"
-
-  pkg_json="${node_pkg_name}/package.json"
-
-  # Check for workspaces in the package.json
-  if [ "$(jq -e ".workspaces | type== \"array\"" "$pkg_json")" == "true" ]; then
-    readarray -t node_pkg_workspaces < <(jq -r ".workspaces[]" "$pkg_json")
-    node_pkg_workspaces=("${node_pkg_workspaces[@]}")
+fi
+if [ ${#node_pkg_workspaces[@]} -gt 0 ]; then
     printf "%b Found Node package workspaces for: %s\n" ${UNICORN_EMOJI} "$(IFS=' '; echo "${node_pkg_workspaces[*]}")"
-  fi
-
-  # Ensure node_modules exist, else create them, as volumes will be mounted there
-  mkdir -p "${node_pkg_name}"/node_modules
-  for workspace in "${node_pkg_workspaces[@]}"; do
-    mkdir -p "${node_pkg_name}/${workspace}"/node_modules
-  done
 fi
 
 trap "cleanup" EXIT
@@ -504,7 +507,7 @@ sed "s#{{ mounted_python_modules }}#${json}#g" "${demo_dir}/values.yaml.bak" > "
 mv "${demo_dir}/values.yaml" "${demo_dir}/values.yaml.bak"
 
 # Add the node package and its workspaces
-sed "s/{{ node_module_to_mount }}/${node_pkg_name}/g" "${demo_dir}/values.yaml.bak" > "${demo_dir}/values.yaml"
+sed "s#{{ node_module_to_mount }}#${node_pkg_name}#g" "${demo_dir}/values.yaml.bak" > "${demo_dir}/values.yaml"
 mv "${demo_dir}/values.yaml" "${demo_dir}/values.yaml.bak"
 
 json="["
